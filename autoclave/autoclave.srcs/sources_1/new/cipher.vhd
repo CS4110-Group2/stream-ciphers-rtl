3400d4339ff06eb5b1f08b509b2d6ff53fb1f86c
Vetle <proffesional_weirdo@hotmail.com>
2021-11-26 22:05:33 +0100

    Blackboxed Autoclave
    Removed all unnecessary files and made adjustments so Autoclave gives off the smallest footprint i could make.

--- autoclave/autoclave.srcs/sources_1/new/cipher.vhd
+++ autoclave/autoclave.srcs/sources_1/new/cipher.vhd
@@ -3,7 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity cipher is
-    Port ( ascii_r : in STD_LOGIC_VECTOR (7 downto 0);
+    Port ( en, load : in STD_LOGIC; 
+         ascii_r : in STD_LOGIC_VECTOR (7 downto 0);
          key : in STD_LOGIC_VECTOR (7 downto 0);
          cphr_out : out STD_LOGIC_VECTOR (7 downto 0);
          encrypt : in STD_LOGIC );
@@ -18,7 +19,8 @@ begin
     sdin <= unsigned(ascii_r);
     skey <= unsigned(key);
 
-    sdout <= -- case of space:
+    sdout <= sdout when en /= '1' or load /= '1' else
+             -- case of space:
              x"20" when ( sdin=x"20" ) else
              -- Encrypting
              -- case of uppercase:
@@ -30,9 +32,10 @@ begin
              -- case of uppercase:
              ( ( ( 26 + ( sdin - x"41" ) - ( skey - x"41" ) ) MOD 26 ) + x"41" ) when ( ( ( sdin >= x"41" ) and ( sdin <= x"5A" ) ) ) else
              -- case of lowercase (add make it uppercase):
-             ( ( ( 26 + ( sdin - x"61" ) - ( skey - x"41" ) ) MOD 26 ) + x"41" ) when ( ( ( sdin >= x"61" ) and ( sdin <= x"7A" ) ) ) else
+             ( ( ( 26 +( sdin - x"61" ) - ( skey - x"41" ) ) MOD 26 ) + x"41" ) when ( ( ( sdin >= x"61" ) and ( sdin <= x"7A" ) ) ) else
              sdin;
 
     cphr_out <= std_logic_vector(sdout);
+--    cphr_out <= std_logic_vector(sdout) when sdout >= 0 else std_logic_vector(sdout + 26);
 
 end arch;
